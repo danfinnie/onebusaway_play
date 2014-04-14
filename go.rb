079949c243a988e17a8a1b4e3a3a6508183abd11
Dan Finnie <dan@danfinnie.com>
2014-04-13 23:09:57 -0400

    Attempt at fixing some timezone issues.

--- go.rb
+++ go.rb
@@ -12,6 +12,7 @@ java_import 'org.onebusaway.gtfs.impl.calendar.CalendarServiceImpl'
 java_import 'org.jruby.util.JRubyFile'
 java_import 'java.util.GregorianCalendar'
 java_import 'java.util.Calendar'
+java_import 'java.util.TimeZone'
 java_import 'java.text.SimpleDateFormat'
 
 store = GtfsRelationalDaoImpl.new
@@ -24,21 +25,30 @@ factory = CalendarServiceDataFactoryImpl.new(store)
 calendar_service = CalendarServiceImpl.new(factory.createData)
 
 get '/trains' do
-  now = GregorianCalendar.new
   # start_of_day = GregorianCalendar.new(now.get(Calendar::YEAR), now.get(Calendar::MONTH), now.get(Calendar::DAY_OF_MONTH))
-  service_date = ServiceDate.new(now)
+  service_date = ServiceDate.new
   services = calendar_service.get_service_ids_on_date(service_date)
 
   date_format = SimpleDateFormat.new
 
   res = services.map do |service|
+    agency = store.get_agency_for_id(service.agency_id)
+    timezone = TimeZone.getTimeZone(agency.timezone)
+    now = service_date.getAsCalendar(timezone)
+
     store.get_trips_for_service_id(service).map do |trip|
       catch(:found_position) do
         store.get_stop_times_for_trip(trip).each_cons(2) do |from_stop_time, to_stop_time|
-          # Minus ones below are because the service date class adds one to the month...ewww
-          from_time = GregorianCalendar.new(service_date.year, service_date.month - 1, service_date.day, 0, 0, from_stop_time.departure_time)
-          to_time = GregorianCalendar.new(service_date.year, service_date.month - 1, service_date.day, 0, 0, to_stop_time.arrival_time)
-          # The timezone below is wrong.
+          from_time = now.clone
+          ServiceDate.moveCalendarToServiceDate(from_time)
+          from_time.add(Calendar::SECOND, from_stop_time.departure_time);
+          from_time.add(Calendar::HOUR, -4) # Bad
+
+          to_time = now.clone
+          ServiceDate.moveCalendarToServiceDate(to_time)
+          to_time.add(Calendar::SECOND, to_stop_time.arrival_time);
+          to_time.add(Calendar::HOUR, -4) # Bad
+
           if from_time.compare_to(now) < 0 and to_time.compare_to(now) > 0
             percent_complete = (now.getTimeInMillis - from_time.getTimeInMillis).to_f / (to_time.getTimeInMillis - from_time.getTimeInMillis)
             lat = from_stop_time.stop.lat + (to_stop_time.stop.lat - from_stop_time.stop.lat) * percent_complete
