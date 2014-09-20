81dcbc27b6e9f18298ee78081b89a111b9e02f89
Dan Finnie <dan@danfinnie.com>
2014-09-20 11:13:49 -0700

    Require at least one file to be imported.

--- import.rb
+++ import.rb
@@ -7,5 +7,7 @@ require_relative 'lib/importer/directory_importer'
 require_relative 'lib/importer/zip_directory_importer'
 require_relative 'lib/importer/importer'
 
+raise "Must select at least one file to import" if ARGV.empty?
+
 db = SQLite3::Database.new "db.db"
 Importer::Importer.new(db, ARGV).import!
